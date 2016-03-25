8c7422f8024633d066352119a7ff45fe227c0483
Ken Kundert <thenurd@nurdletech.com>
2016-03-25 10:04:45 -0700

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='vdiff',
-    version='2.3.1',
+    version='2.3.2',
     author='Ken Kundert',
     author_email='vdiff@nurdletech.com',
     description='Efficiently manage the differences between two files using vim.',
