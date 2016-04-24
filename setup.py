1e1f7cd76e696c87114abeb2f0a536161c6989ec
Ken Kundert <thenurd@nurdletech.com>
2016-04-24 11:55:15 -0700

    Bump version: 2.3.6 → 2.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='vdiff',
-    version='2.3.6',
+    version='2.4.0',
     author='Ken Kundert',
     author_email='vdiff@nurdletech.com',
     description='Efficiently manage the differences between two files using vim.',
--- vdiff/__init__.py
+++ vdiff/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '2.3.6'
+__version__ = '2.4.0'
 
 from .vdiff import Vdiff
